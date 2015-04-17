f51dc6f4109600552a1ebc267bc062bb9a752b49
takeru0911 <takeru911run@gmail.com>
2015-04-17 19:18:35 +0900

    model,view作成

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,32 @@
 package controllers;
 
 import play.*;
+import play.data.*;
 import play.mvc.*;
 
+
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
+    static Form<Task> taskForm = Form.form(Task.class);
 
     public static Result index() {
         return ok("Hello World");
     }
+
+    public static Result tasks() {
+	return ok(
+		  views.html.index.render(Task.all(), taskForm)
+		  );
+    }
+
+    public static Result newTask() {
+	return TODO;
+    }
+
+    public static Result deleteTask(Long id){
+	return TODO;
+    }
+      
 }
---
+++ app/models/Task.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.*;
+
+import play.data.validation.Constraints.*;
+
+public class Task{
+
+    public Long id;
+    @Required
+    public String label;
+
+    public static List<Task> all(){
+	return new ArrayList<Task>();
+    }
+
+    public static void create(Task task) {
+    }
+
+    public static void delete(Long id) {
+    }
+
+}
